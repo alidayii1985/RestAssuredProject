2e36c417dfb6b4adb9fc31bfb2f7ac58073707c8
github@cybertekschool.com <github@cybertekschool.com>
2020-01-07 12:47:27 -0500

    Initial commit for Rest Assured Project

---
+++ src/test/java/spartans/SimpleTest.java
@@ -0,0 +1,18 @@
+package spartans;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    public void test1(){
+
+        System.out.println("Hello");
+        int x = 9 ;
+        assertEquals(x , 9) ;
+
+    }
+
+}
