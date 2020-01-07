f369b8c0fd8dd984d96792c6ce687543c9cde2fc
github@cybertekschool.com <github@cybertekschool.com>
2020-01-07 13:07:11 -0500

    Added the log for request and response

--- src/test/java/spartans/SpartanAPITest.java
+++ src/test/java/spartans/SpartanAPITest.java
@@ -1,5 +1,7 @@
 package spartans;
 
+import io.restassured.RestAssured;
+import org.junit.Before;
 import org.junit.Test;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
@@ -7,13 +9,29 @@ import static org.hamcrest.Matchers.*;
 
 public class SpartanAPITest {
 
+    @Before
+    public void setUp(){
+
+        RestAssured.baseURI = "http://107.20.25.103:8000/api" ;
+
+    }
+
     @Test
     public void getSingleSpartanTest(){
 
+        given()
+                .log().all().
         when()
-                .get("http://107.20.25.103:8000/api/spartans/395").
+                .get("/spartans/395").
         then()
+                .log().all()
                 .statusCode(200) ;
+
+    }
+
+    @Test
+    public void verifySpartanDataTest(){
+
 
 
     }
